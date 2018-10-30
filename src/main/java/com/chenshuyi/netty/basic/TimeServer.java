c6faa8218aee4814ff7bdc5cfdd2d3190016cd24
yurong01.chen <yurong01.chen@vipshop.com>
2018-10-30 20:01:25 +0800

    添加日志

--- src/main/java/com/chenshuyi/netty/basic/TimeServer.java
+++ src/main/java/com/chenshuyi/netty/basic/TimeServer.java
@@ -26,6 +26,7 @@ public class TimeServer {
                     .childHandler(new ChildChannelHandler());
             // 绑定端口，同步等待成功
             ChannelFuture f = b.bind(port).sync();
+            System.out.println("The time server is start in port : " + port);
             // 等待服务端监听端口关闭
             f.channel().closeFuture().sync();
         } finally {
@@ -48,13 +49,6 @@ public class TimeServer {
      */
     public static void main(String[] args) throws Exception {
         int port = 8080;
-        if (args != null && args.length > 0) {
-            try {
-                port = Integer.valueOf(args[0]);
-            } catch (NumberFormatException e) {
-                // 采用默认值
-            }
-        }
         new TimeServer().bind(port);
     }
 }
